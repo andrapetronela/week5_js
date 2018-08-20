23a37be2c4e9201202fda0d859e59847458528e8
Andra <andra.popa1989@gmail.com>
2018-08-20 13:01:25 +0100

    leap years

--- script.js
+++ script.js
@@ -160,4 +160,22 @@
 
 
 
+//
+//Leap years. Write a program that asks for a starting year and an ending year and then puts all the leap years between them (and including them, if they are also leap years). Leap years are years divisible by 4 (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are also divisible by 400 (such as 1600 and 2000, which were in fact leap years). What a mess!
+//
+
+//
+//
+//firstYear = parseInt(prompt("Please enter the first year: "));
+//secondYear = parseInt(prompt("Please enter the second year: "));
+//
+//for (var x = firstYear; x <= secondYear; x++) {
+//    if (x % 400 == 0) {
+//        console.log(x);
+//    } 	
+//	else if (x % 4 == 0 && x % 100 != 0) {
+//        console.log(x);
+//    }
+//}
+
 
